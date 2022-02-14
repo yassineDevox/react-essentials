93d374bdbd25a82c1095ff56b476b16682753cbf
yassineDevox <yassine.rassy1@gmail.com>
2022-02-14 23:12:32 +0100

    local variable problem

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import React, { Component } from 'react'
 
+export default class App extends Component {
 
-function App() {
-  return (
-    <h1>Hello Amine 😃 !!</h1>
-  );
+  constructor() {
+    super()
+    this.counter = 0
+    console.log(typeof this.counter)
+  }
+
+  handleClick = () => {
+    this.counter++
+    console.log(this.counter)
+  }
+
+  render() {
+    return (
+      <>
+        <div className="text-center p-5">
+          <div className='text-center m-2'>{this.counter} 😃 !!</div>
+          <button
+            onClick={this.handleClick}
+            className="btn btn-success">+</button>
+        </div>
+      </>
+    )
+  }
 }
-
-export default App;
