bf77aeed6f071ae1bb4f30318eebd3fee01104f3
yassineDevox <yassine.rassy1@gmail.com>
2022-08-20 19:00:40 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,32 +1,7 @@
-import { useState } from "react"
-import { initialState, todoActions, todoReducer } from "./reducers/todo";
+import React from 'react'
 
-
-function App() {
-
-  const [action, setAction] = useState(initialState)
-
+export const App = () => {
   return (
-    <>
-      <h1>Action : 😃 !!</h1>
-      <button onClick={() => {
-        const newTodo = prompt("new Todo : ")
-        setAction(
-          todoReducer(action,{ type: todoActions.ADD, payload: { data: newTodo } })
-        )
-      }}>save </button>
-      <button onClick={() => {
-        const updatedVal = prompt("updated todo : ")
-        setAction(todoReducer(action,{ type: todoActions.UPDATE, payload: { updatedVal, id: 1 } }))
-      }}>update</button>
-      <button onClick={() => {
-        setAction(todoReducer(action,{ type: todoActions.DELETE, payload: { deletedId: 1 } }))
-      }}>delete</button>
-      <button onClick={() => {
-        setAction(todoReducer(action,{ type: todoActions.DETAILS, payload: { id: 1 } }))
-      }}>more details</button>
-    </>
-  );
+    <div>App</div>
+  )
 }
-
-export default App;
