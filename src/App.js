b264c423a0dea1e907ac58a5ca50c11d349d36e9
yassineDevox <yassine.rassy1@gmail.com>
2022-08-20 02:01:51 +0100

    clean master branch

--- src/App.js
+++ src/App.js
@@ -1,32 +1,25 @@
-import { useState } from "react"
-import { initialState, todoActions, todoReducer } from "./reducers/todo";
+import PropTypes from 'prop-types';
 
+const ColorComp = ({ color }) => {
+  return <> {color}</>
+}
 
 function App() {
-
-  const [action, setAction] = useState(initialState)
-
+  // error hooks can't be declared inside if 
+  // if (true) {
+  //   const [first, setfirst] = useState("second")
+  // }
   return (
-    <>
-      <h1>Action : 😃 !!</h1>
-      <button onClick={() => {
-        const newTodo = prompt("new Todo : ")
-        setAction(
-          todoReducer(action,{ type: todoActions.ADD, payload: { data: newTodo } })
-        )
-      }}>save </button>
-      <button onClick={() => {
-        const updatedVal = prompt("updated todo : ")
-        setAction(todoReducer(action,{ type: todoActions.UPDATE, payload: { updatedVal, id: 1 } }))
-      }}>update</button>
-      <button onClick={() => {
-        setAction(todoReducer(action,{ type: todoActions.DELETE, payload: { deletedId: 1 } }))
-      }}>delete</button>
-      <button onClick={() => {
-        setAction(todoReducer(action,{ type: todoActions.DETAILS, payload: { id: 1 } }))
-      }}>more details</button>
-    </>
+    <ColorComp />
   );
+}
+
+ColorComp.propTypes = {
+  color: PropTypes.string.isRequired
+};
+
+ColorComp.defaultProps = {
+  color: "red"
 }
 
 export default App;
