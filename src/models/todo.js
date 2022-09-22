bd0f7b7f6bb6b2eae9016042eb3351dd6fc9e720
yassineDevox <yassine.rassy1@gmail.com>
2022-09-22 02:42:54 +0100

    first commit

--- src/models/todo.js
+++
@@ -1,8 +0,0 @@
-export class TodoModel {
-    constructor(id, title, completed = false,userId=1) {
-        this.id = id
-        this.title = title
-        this.completed = completed
-        this.userId = userId
-    }
-}
--- src/pages/index.js
+++
@@ -1,5 +0,0 @@
-export { AddPage } from "./add-todo";
-export { ListPage } from "./list-todo";
-export { EditPage } from "./edit-todo";
-export { DetailsPage } from "./details-todo";
-
