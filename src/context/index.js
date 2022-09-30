a6eaa221ca802ee608d4fbe82e88217d9061c632
yassineDevox <yassine.rassy1@gmail.com>
2022-10-01 00:16:31 +0100

    is totaly possible to track the builder and even a property neasted using useEffect

--- src/context/index.js
+++ src/context/index.js
@@ -1,10 +1,19 @@
-import React, { useReducer, useState } from 'react'
+import React, { useEffect, useReducer } from 'react'
 import { builderReducer, initialState } from '../reducers/builder'
 
 export const builderContext = React.createContext()
 
 export const BuilderProvider = ({ children }) => {
 const [builder, dispatch] = useReducer(builderReducer,initialState)
+  
+  useEffect(() => {
+    console.log("builder changed",builder)
+  }, [builder])
+  
+  useEffect(() => {
+    console.log("current device changed",builder)
+  }, [builder.currentDevice])
+  
   return (
     <builderContext.Provider
       value={{ builder,dispatch }}
