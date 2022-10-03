d5b9d0a83516fe8fa31fd93f16157dcbc78284db
yassineDevox <yassine.rassy1@gmail.com>
2022-10-04 00:36:05 +0100

    fix design issues -input component

--- src/input/index.js
+++ src/input/index.js
@@ -1,20 +1,14 @@
 import React from 'react'
-
+import inputStyle from "./input.module.css"
 export const Input = ({ placeholder, label }) => {
   return (
-    <div style={{ margin: 10, color: '#303f9f', fontFamily: 'monospace' }}>
+    <div className={inputStyle.container}>
       <label style={{ fontWeight: 'bold' }}>{label}</label>
       <br />
       <textarea
         placeholder={placeholder}
         type='text'
-        style={{
-          padding: 5,
-          border: '2px solid #303f9f',
-          width: '100%',
-          backgroundColor: '#80808017',
-          height: '60px'
-        }}
+        className={inputStyle.inpt}
       />
     </div>
   )
